822671565f2e194187e5c6511ba3d92dbe795566
Aviel Oiknine <avieloikpro@gmail.com>
2025-12-24 13:03:05 +0200

    fix: comment out unused authentication middleware in app.js

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ import { readJsonFile, writeJsonFile } from './filesFunction.js';
 const app = express();
 const port = 3000
 
-app.use(async (req, res, next) => {
-    const userName = req.header('X-User-Name')
-    const password = req.header('X-Password')
-    const users = await readJsonFile('./data/users.json')
-    const user = users.find(u => u.userName === userName && u.password === password)
-    user ? next() : res.status(401).send('Unauthorized')
-})
+// app.use(async (req, res, next) => {
+//     const userName = req.header('X-User-Name')
+//     const password = req.header('X-Password')
+//     const users = await readJsonFile('./data/users.json')
+//     const user = users.find(u => u.userName === userName && u.password === password)
+//     user ? next() : res.status(401).send('Unauthorized')
+// })
 app.use(express.json());
 app.use('/reports', reportsRouter);
 app.use('/users', usersRouter);
